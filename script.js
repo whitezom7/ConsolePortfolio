115d2ea062e8482f13f4127fe7b0dd69c75b2752
Taylor <taylor.whitewood2022@gmail.com>
2023-05-26 01:12:28 +0200

    Changed Terminal color

--- script.js
+++ script.js
@@ -1,50 +1,43 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var terminalInput = document.getElementById('terminal-input');
-    var outputDiv = document.getElementById('output');
-    var usernameElement = document.getElementById('username');
-    var username = 'taylor@whitewood'; // Default username
-  
-    // Set default command
-    terminalInput.value = 'cat help.md';
-  
-    terminalInput.addEventListener('keyup', function (event) {
-      if (event.keyCode === 13) {
-        event.preventDefault();
-        processCommand(terminalInput.value);
-        terminalInput.value = '';
-      }
-    });
-  
-    function processCommand(command) {
-      // Append user command to the output
-      var commandElement = document.createElement('p');
-      commandElement.textContent = username + ':$ ' + command;
-      outputDiv.appendChild(commandElement);
-  
-      // Process the command
-      if (command === 'cat aboutme.md') {
-        outputDiv.innerHTML += loadMarkdownFile(`Markdowns/aboutme.md`, `output`);
-     // } else if (command === 'cat projects.md') {
-        //outputDiv.innerHTML += loadMarkdownFile(`Markdowns/projects.md`, `output`);
-    } else if (command === 'cat contact.md') {
-        outputDiv.innerHTML += loadMarkdownFile(`Markdowns/contact.md`, `output`);
-    } else if (command === 'clear') {
+document.addEventListener('DOMContentLoaded', function() {
+  var terminalInput = document.getElementById('terminal-input');
+  var outputDiv = document.getElementById('output');
+  var usernameElement = document.getElementById('username');
+  var username = 'taylor@whitewood'; // Default username
+
+  // Set default command
+  terminalInput.value = 'cat help.md';
+
+  terminalInput.addEventListener('keyup', function(event) {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      processCommand(terminalInput.value);
+      terminalInput.value = '';
+    }
+  });
+
+  function processCommand(command) {
+    // Append user command to the output
+    var commandElement = document.createElement('p');
+    commandElement.innerHTML = `<span style="color: green;" id="username">${username}:$</span> ${command}`;
+    outputDiv.appendChild(commandElement);
+
+    let userInput = command.toLowerCase();
+    // Process the command
+      if (userInput === 'cat aboutme.md' || userInput === '-cat aboutme.md') {
+        outputDiv.innerHTML += loadMarkdownFile('Markdowns/aboutme.md', 'output');
+      } else if (userInput === 'cat contact.md' || userInput === '-cat contact.md') {
+        outputDiv.innerHTML += loadMarkdownFile('Markdowns/contact.md', 'output');
+      } else if (userInput === 'clear' || userInput === '-clear') {
         clear();
-      } else if (command === 'cat help.md') {
-        outputDiv.innerHTML += '<p>Commands:</p>';
-        outputDiv.innerHTML += '<p>-cat aboutme.md: Displays my about me!</p>';
-        // outputDiv.innerHTML += '<p>-cat projects.md: Shows my projects!</p>';
-        outputDiv.innerHTML += '<p>-cat contact.md: You can contact me here!</p>';
-        outputDiv.innerHTML += `<p>-clear: Clears the terminal output.</p>`;
+      } else if (userInput === 'help' || userInput === 'cat help.md' || userInput === '-cat help.md') {
+        showHelp();
       } else {
         outputDiv.innerHTML += '<p>Invalid command!</p>';
-      }
-  
-      // Scroll to the bottom of the terminal
-      outputDiv.scrollTop = outputDiv.scrollHeight;
     }
-  
-  
+
+    // Scroll to the bottom of the terminal
+    outputDiv.scrollTop = outputDiv.scrollHeight;
+  }
 
   function loadMarkdownFile(filename, targetId) {
     // Fetch the Markdown file
@@ -73,4 +66,19 @@ document.addEventListener('DOMContentLoaded', function () {
     const outputDiv = document.getElementById('output');
     outputDiv.innerHTML += `${message}<br>`;
   }
-})
+});
+
+function showHelp() {
+  // Access the outputDiv element by its id
+  var outputDiv = document.getElementById('output');
+
+  // Create the help content as a single HTML string
+  var helpContent = '';
+  helpContent += '<p>Commands:</p>';
+  helpContent += '<p>-cat aboutme.md: Displays my about me!</p>';
+  helpContent += '<p>-cat contact.md: You can contact me here!</p>';
+  helpContent += '<p>-clear: Clears the terminal output.</p>';
+
+  // Append the help content to the outputDiv
+  outputDiv.innerHTML += helpContent;
+}
